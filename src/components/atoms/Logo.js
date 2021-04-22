685032efeb9f0fbdfe90d09342a5d734c926219c
sebastian-lucjan <sebastian.lucjan@gmail.com>
2021-04-22 12:57:37 +0200

    Base project config - eslint, prettier, huskey, lintstaged

---
+++ src/components/atoms/Logo.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Logo = () => {
+  return (
+    <>
+      <h1>minimalist</h1>
+    </>
+  );
+};
---
+++ src/views/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { Logo } from 'components/atoms/Logo';
+
+const App = () => {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <Logo />
+      </header>
+    </div>
+  );
+};
+
+export default App;
